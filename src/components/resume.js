69a98554afef20639235af168665a27c592a4d0d
pebbies <0stonewang0@gmail.com>
2024-09-21 16:57:09 -0400

    Quick Fix Resume Page

--- src/components/resume.js
+++ src/components/resume.js
@@ -16,7 +16,7 @@ class Resume extends Component {
         </div>
 
         <div className="mt-4 flex flex-row items-center">
-          Novice Videographer on"Easy watches" often get dismissed as low-effort entertainment, something to zone out to without expecting much in return. Don't expect to watch these shows and have a changed outlook on life. But the next time you find yourself reaching for the familiar, the kind of familiar you turn to when you're feeling vulnerable, take a moment to consider why. If these are the stories that speak to you when you're vulnerable, when you’re truly yourself, maybe they have something important to say about who you are.
+          Novice Videographer on
           <Tiktok />
         </div>
       </div>
