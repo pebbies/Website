d9ec0e0ca48f77aa0bfd2753a8f3176a4a662add
pebbies <0stonewang0@gmail.com>
2024-10-10 13:29:47 -0400

    new certificate plus emojis

--- src/components/resume.js
+++ src/components/resume.js
@@ -6,27 +6,27 @@ class Resume extends Component {
     return (
       <div className="mt-8">
         <div className="mt-4 flex flex-row items-center">
-          Former Investment Banker at
+          🏦 Former Investment Banker at
           <div className="font-semibold ml-2">Nomura</div>
         </div>
 
         <div className="mt-4 flex flex-row items-center">
-          BA in Economics, Minor in Computer Science from
+          🎓 BA in Economics, Minor in Computer Science from
           <div className="font-semibold ml-2">Northwestern University</div>
         </div>
 
         <div className="mt-4 flex flex-row items-center">
-          Certificate in AR/VR: Technologies and Applications from
-          <div className="font-semibold ml-2">University of Michigan (Coursera)</div>
+          👾 Certificate in AR/VR: Technologies and Applications from
+          <div className="font-semibold ml-2">University of Michigan</div>
         </div>
 
-        {/* <div className="mt-4 flex flex-row items-center">
-          Certificate in XR Development from
-          <div className="font-semibold ml-2">University of Michigan (Coursera)</div>
-        </div> */}
+        <div className="mt-4 flex flex-row items-center">
+          🕹️ Certificate in XR Development from
+          <div className="font-semibold ml-2">University of Michigan</div>
+        </div>
 
         <div className="mt-4 flex flex-row items-center">
-          Novice Videographer on
+          📹 Novice Videographer on
           <Tiktok />
         </div>
       </div>
